42bc988d79638002759bb9dff255e1901fa93dba
shermondevelopment <victorshermon@yahoo.com>
2022-07-30 14:56:07 -0300

    should remove 1 point to recommendation and delete recommendation if score bellow -5

--- tests/units/recommendationsService.test.ts
+++ tests/units/recommendationsService.test.ts
@@ -58,12 +58,32 @@ describe('Recommendation', () => {
   })
 
   describe("downvote recommendation", () => {
-    it("should remove 1 point to recomendaiton and delete recomnendation if score bellow -5", async () =>{
+    it("should fail add 1 point to recommendation score if id doesn't exist", async () =>{
       jest.spyOn(recommendationRepository, "find").mockResolvedValueOnce(null)
       expect(recommendationService.upvote(100)).rejects.toEqual({
         type: 'not_found',
         message: ''
       })
     })
+    
   })
+
+  describe("downvote recommendation", () => {
+    it("should remove 1 point to recommendation and delete recommendation if score bellow -5", async () => {
+      const recommendation = {
+        id: 1,
+        name: faker.lorem.words(3),
+        youtubeLink: 'https://www.youtube.com/watch?v=U0d0xpjCjWo',
+        score: -5,
+      }
+
+      jest.spyOn(recommendationRepository, "find").mockResolvedValueOnce(recommendation)
+      jest.spyOn(recommendationRepository, "updateScore").mockResolvedValueOnce({ ...recommendation, score: -6 })
+      jest.spyOn(recommendationRepository, "remove").mockResolvedValueOnce()
+
+      await recommendationService.downvote(recommendation.id)
+      expect(recommendationRepository.remove).toBeCalledTimes(1)
+    })
+  })
+
 })
