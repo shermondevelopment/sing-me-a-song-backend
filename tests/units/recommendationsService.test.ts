eefcbbd5586e4fffd0070e71b723f2169e625fef
shermondevelopment <victorshermon@yahoo.com>
2022-07-30 14:31:02 -0300

    should throw a conflict error if the name of the recommendation is not unique

--- tests/units/recommendationsService.test.ts
+++ tests/units/recommendationsService.test.ts
@@ -16,4 +16,18 @@ describe('Recommendation', () => {
     await recommendationService.insert(recommendation)
     expect(recommendationRepository.create).toBeCalledTimes(1)
   })
+
+  it('should throw a conflict error if the name of the recommendation is not unique', async() => {
+    const recommendation = {
+      name: faker.lorem.words(3),
+      youtubeLink: "https://www.youtube.com/watch?v=U0d0xpjCjWo"
+    }
+
+    jest.spyOn(recommendationRepository, "findByName")
+    .mockResolvedValueOnce({ id: 1, ...recommendation, score: 0 })
+    expect(recommendationService.insert(recommendation)).rejects.toEqual({
+      type: "conflict",
+      message: "recommendations names must be unique"
+    })
+  })
 })
